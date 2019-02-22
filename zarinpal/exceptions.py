989a91748704c2c19b89f7740b1eddf43df8b34f
glyphack <sh.hooshyari@gmail.com>
2019-02-22 11:31:02 +0330

    [zarinpal]: Created exceptions

---
+++ zarinpal/exceptions.py
@@ -0,0 +1,16 @@
+class Error(Exception):
+    """
+    BaseClass for exceptions
+    """
+
+
+class UnknownTransactionFailure(Error):
+    """ Unknown transaction failure """
+
+
+class UnknownTransactionStatusCode(Error):
+    """ Unknown transaction status code """
+
+
+class CallbackUrlNotProvided(Error):
+    """ callback url is not provided in transaction data or project settings """
