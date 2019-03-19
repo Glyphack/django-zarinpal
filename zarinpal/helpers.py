cd8bd6cada5948df16d848d251b69e63e87cb07f
glyphack <sh.hooshyari@gmail.com>
2019-03-19 17:09:55 +0330

    [yekpay]: added helper function to reformat start transaction data

---
+++ zarinpal/helpers.py
@@ -0,0 +1,13 @@
+from .config import ZARINPAL_MERCHANT_ID
+from .utils import get_call_back_url
+
+
+def generate_start_transaction_data(transaction):
+    return {
+        'merchant_id': ZARINPAL_MERCHANT_ID,
+        "amount": transaction.amount,
+        "description": transaction.description,
+        "email": transaction.email,
+        "mobile": transaction.mobile,
+        "callback_url": get_call_back_url(transaction),
+    }
