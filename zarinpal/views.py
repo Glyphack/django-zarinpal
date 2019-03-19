1c7e2c371bebe442e87afb893adb6a4fd21db681
glyphack <sh.hooshyari@gmail.com>
2019-03-19 17:08:26 +0330

    [yekpay]: created function for verifying transaction after redurect from url

--- zarinpal/views.py
+++ zarinpal/views.py
@@ -1,38 +1,34 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    UpdateView,
-    ListView
-)
+from django.http import HttpResponse
+from zeep import Client
 
+from zarinpal.config import ZARINPAL_WEBSERVICE, ZARINPAL_MERCHANT_ID
 from .models import (
-	Transaction,
+    Transaction,
 )
 
-
-class TransactionCreateView(CreateView):
-
-    model = Transaction
+from .utils import verify_transaction
 
 
-class TransactionDeleteView(DeleteView):
-
-    model = Transaction
-
-
-class TransactionDetailView(DetailView):
-
-    model = Transaction
-
-
-class TransactionUpdateView(UpdateView):
-
-    model = Transaction
-
-
-class TransactionListView(ListView):
-
-    model = Transaction
-
+def verify_transaction_view(request):
+    client = Client(ZARINPAL_WEBSERVICE)
+    authority = request.GET['Authority']
+    transaction = Transaction.objects.get(authority=authority)
+    if not transaction:
+        return HttpResponse('this request does not have a started transaction')
+    if request.GET.get("Status") == "OK":
+        result = client.service.PaymentVerification(
+            ZARINPAL_MERCHANT_ID, authority, transaction.amount
+        )
+        if result.Status == 100:
+            transaction.success(result.RefID)
+            return HttpResponse('Transaction success. RefID: ' + str(result.RefID))
+        elif result.Status == 101:  # todo: inspect situation
+            transaction.status = result.Status
+            return HttpResponse('Transaction submitted : ' + str(result.Status))
+        else:  # todo: inspect situation
+            transaction.status = result.Status
+            return HttpResponse('Transaction failed. Status: ' + str(result.Status))
+    else:
+        transaction.fail('Canceled')
+        return HttpResponse('Transaction failed or canceled by user')
