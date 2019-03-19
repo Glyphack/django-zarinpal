d0f6332333b3c6c32455e8bb9f5c006d5bf14613
glyphack <sh.hooshyari@gmail.com>
2019-03-19 17:04:20 +0330

    [yekpay]: added url for verify_transaction_view

--- zarinpal/urls.py
+++ zarinpal/urls.py
@@ -1,35 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
-from . import views
+from .views import verify_transaction_view
 
 
 app_name = 'zarinpal'
 urlpatterns = [
-    url(
-        regex="^Transaction/~create/$",
-        view=views.TransactionCreateView.as_view(),
-        name='Transaction_create',
-    ),
-    url(
-        regex="^Transaction/(?P<pk>\d+)/~delete/$",
-        view=views.TransactionDeleteView.as_view(),
-        name='Transaction_delete',
-    ),
-    url(
-        regex="^Transaction/(?P<pk>\d+)/$",
-        view=views.TransactionDetailView.as_view(),
-        name='Transaction_detail',
-    ),
-    url(
-        regex="^Transaction/(?P<pk>\d+)/~update/$",
-        view=views.TransactionUpdateView.as_view(),
-        name='Transaction_update',
-    ),
-    url(
-        regex="^Transaction/$",
-        view=views.TransactionListView.as_view(),
-        name='Transaction_list',
-    ),
+        path("/verify/<str:>", verify_transaction_view, name="verify")
 	]
