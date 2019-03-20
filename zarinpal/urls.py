1c074e5834d956b87e1514ef43eb4d8d1cfa9685
glyphack <sh.hooshyari@gmail.com>
2019-03-20 18:47:31 +0330

    [zarinpal]: code reformatted.

--- zarinpal/urls.py
+++ zarinpal/urls.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
 from django.urls import path
-from django.views.generic import TemplateView
 
 from .views import verify_transaction_view
 
-
 app_name = 'zarinpal'
 urlpatterns = [
-        path("/verify/<str:>", verify_transaction_view, name="verify")
-	]
+    path("/verify", verify_transaction_view, name="verify")
+]
