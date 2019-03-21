5ca7649029d156c9af0f1949363e186ed3723ead
glyphack <sh.hooshyari@gmail.com>
2019-03-21 15:47:37 +0330

    [zarinpal]: departed verify transaction view verification into another function.

--- zarinpal/views.py
+++ zarinpal/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from zeep import Client
 
 from zarinpal.config import ZARINPAL_WEBSERVICE, ZARINPAL_MERCHANT_ID
@@ -8,27 +9,14 @@ from .models import (
 )
 
 from .utils import verify_transaction
+from .signals import transaction_verified
 
 
 def verify_transaction_view(request):
-    client = Client(ZARINPAL_WEBSERVICE)
-    authority = request.GET['Authority']
-    transaction = Transaction.objects.get(authority=authority)
-    if not transaction:
-        return HttpResponse('this request does not have a started transaction')
-    if request.GET.get("Status") == "OK":
-        result = client.service.PaymentVerification(
-            ZARINPAL_MERCHANT_ID, authority, transaction.amount
-        )
-        if result.Status == 100:
-            transaction.success(result.RefID)
-            return HttpResponse('Transaction success. RefID: ' + str(result.RefID))
-        elif result.Status == 101:  # todo: inspect situation
-            transaction.status = result.Status
-            return HttpResponse('Transaction submitted : ' + str(result.Status))
-        else:  # todo: inspect situation
-            transaction.status = result.Status
-            return HttpResponse('Transaction failed. Status: ' + str(result.Status))
-    else:
-        transaction.fail('Canceled')
-        return HttpResponse('Transaction failed or canceled by user')
+    authority = request.GET.get('Authority')
+    status = request.GET.get("Status")
+    transaction = verify_transaction(status, authority)
+    transaction_verified.send(sender=None, transaction=transaction)
+    return redirect(
+       transaction.callback_url
+    )
