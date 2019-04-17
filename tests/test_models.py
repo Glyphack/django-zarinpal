8726afeb992835afbced54d02c4bf8c0f7947770
glyphack <sh.hooshyari@gmail.com>
2019-04-17 23:40:23 +0430

    [zarinpal]: created test file for testing model functionality.

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,15 +10,17 @@ Tests for `django-zarinpal` models module.
 
 from django.test import TestCase
 
-from zarinpal import models
+from zarinpal.models import Transaction
+from .test_data import valid_transaction_data
+from zarinpal.utils import start_transaction
 
 
 class TestZarinpal(TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
+    def test_creating_simulation_transaction(self):
+        start_transaction(**valid_transaction_data)
 
     def tearDown(self):
         pass
