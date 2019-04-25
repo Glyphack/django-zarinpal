b7d354147137bffe1bb22ab78b426fc8545fa316
glyphack <sh.hooshyari@gmail.com>
2019-04-25 19:03:16 +0430

    removed support for python 2.x.

--- setup.py
+++ setup.py
@@ -64,8 +64,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
