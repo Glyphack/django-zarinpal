69d4c412ec0b99897471f1fb652bd9a75f3508b5
glyphack <sh.hooshyari@gmail.com>
2019-04-25 18:17:26 +0430

    [zarinpal]: used logging instead of print when there is a error with zarinpal api.

--- zarinpal/utils.py
+++ zarinpal/utils.py
@@ -1,15 +1,10 @@
-from django.contrib.sites.models import Site
-from django.http import HttpResponse
-from django.urls import reverse
 from zeep import Client
+import logging
 
-from zarinpal.exceptions import TransactionDoesNotExist
+from zarinpal.helpers import generate_start_transaction_data
 from .config import (
-    ZARINPAL_MERCHANT_ID,
-    ZARINPAL_SIMULATION,
     ZARINPAL_START_GATEWAY,
     ZARINPAL_WEBSERVICE,
-    ZARINPAL_CALLBACK_URL,
 )
 from .models import Transaction
 
@@ -32,49 +27,4 @@ def start_transaction(transaction_data: dict) -> str:
         transaction.save(update_fields=["authority"])
         return ZARINPAL_START_GATEWAY + result.Authority
     else:
-        print(result)
-
-
-def verify_transaction(status: str, authority: int) -> Transaction:
-    client = Client(ZARINPAL_WEBSERVICE)
-    try:
-        transaction = Transaction.objects.get(status="PENDING", authority=authority)
-    except Transaction.DoesNotExist:
-        raise TransactionDoesNotExist
-    if status == "OK":
-        result = client.service.PaymentVerification(
-            ZARINPAL_MERCHANT_ID, authority, transaction.amount
-        )
-        if result.Status == 100:
-            transaction.success(result.RefID)
-        elif result.Status == 101:
-            transaction.status = result.Status
-            return HttpResponse("Transaction submitted : " + str(result.Status))
-        else:
-            transaction.fail(result.Status)
-    else:
-        transaction.fail("Canceled")
-
-    return transaction
-
-
-def generate_start_transaction_data(transaction):
-    return {
-        "merchant_id": ZARINPAL_MERCHANT_ID,
-        "amount": transaction.amount,
-        "description": transaction.description,
-        "email": transaction.email,
-        "mobile": transaction.mobile,
-        "callback_url": get_callback_url(),
-    }
-
-
-def get_callback_url():
-    if ZARINPAL_CALLBACK_URL:
-        return ZARINPAL_CALLBACK_URL
-    else:
-        return (
-            "http://"
-            + Site.objects.get_current().domain
-            + reverse("zarinpal:verify_transaction")
-        )
+        logging.error(result)
