476aa347641ecb5e7163510788af8d023009765f
glyphack <sh.hooshyari@gmail.com>
2019-04-29 11:38:43 +0430

    [zarinpal]: added test for successful start_transaction and not valid amount start transaction.

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,15 +1,25 @@
 from django.test import TestCase
 
 from zarinpal.utils import start_transaction
-from .test_data import valid_transaction_data
+from .test_data import generate_test_transaction_data
+from django.contrib.auth.models import User
+from zarinpal.exceptions import AmountIsLessThanMinimum
 
 
 class TestStartTransaction(TestCase):
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(username='zarinpal-test-user',
+                                             email='zarinpal-test-user-email@example.com',
+                                             password='YouDontKnowMe')
 
-    def test_valid_data_transaction(self):
-        start_transaction(**valid_transaction_data)
+    def test_valid_data_start_transaction(self):
+        start_transaction(generate_test_transaction_data(user=self.user))
+
+    def test_not_valid_amount(self):
+        self.assertRaises(AmountIsLessThanMinimum,
+                          start_transaction,
+                          generate_test_transaction_data(user=self.user, valid_amount=False))
+
 
     def tearDown(self):
-        pass
+        self.user.delete()
