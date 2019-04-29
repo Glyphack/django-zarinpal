024a780bc97c243e820af00bdf766e80bdd29672
glyphack <sh.hooshyari@gmail.com>
2019-04-29 11:37:55 +0430

    [zarinpal]: created function to generate test data based on what kind of data you need.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,1 +1,14 @@
-valid_transaction_data = {}
+def generate_test_transaction_data(user, valid_amount=True):
+    if valid_amount:
+        amount = 2000
+    else:
+        amount = 1
+    return {"user": user,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "amount": amount,
+            "callback_url": None,
+            "description": "charge account",
+            "email": user.email,
+            "mobile": "",
+            }
