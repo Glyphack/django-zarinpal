8657635e4bdce0e8892720173648b0cdd63660a8
glyphack <sh.hooshyari@gmail.com>
2019-04-29 11:39:18 +0430

    [zarinpal]: added exceptions that occur in start_transaction method.

--- zarinpal/exceptions.py
+++ zarinpal/exceptions.py
@@ -1,20 +1,32 @@
-class Error(Exception):
+class ZarinpalError(Exception):
     """
     BaseClass for exceptions
     """
 
 
-class UnknownTransactionFailure(Error):
+class UnknownTransactionFailure(ZarinpalError):
     """ Unknown transaction failure """
 
 
-class UnknownTransactionStatusCode(Error):
+class UnknownTransactionStatusCode(ZarinpalError):
     """ Unknown transaction status code """
 
 
-class CallbackUrlNotProvided(Error):
+class CallbackUrlNotProvided(ZarinpalError):
     """ callback url is not provided in transaction data or project settings """
 
 
-class TransactionDoesNotExist(Error):
+class TransactionDoesNotExist(ZarinpalError):
     """ No transaction submitted with this authority """
+
+
+class TransactionDataIsNotValid(ZarinpalError):
+    """ The data was not valid for Zarinpal gateway"""
+
+
+class CouldNotStartTransaction(ZarinpalError):
+    """did not get start authority from Zarinpal"""
+
+
+class AmountIsLessThanMinimum(ZarinpalError):
+    """minimum amount to start transaction is 1000"""
